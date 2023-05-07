269245d98a08e50e4b5c4cc54498d9d8d35f1be9
kokouapedo <apedokokouagbe@gmail.com>
2023-05-07 12:37:09 -0500

    News change utrain-path

--- terraform_jenkins_aws_ami/aws-ec2-ami.tf
+++ terraform_jenkins_aws_ami/aws-ec2-ami.tf
@@ -7,7 +7,7 @@ provider "aws" {
 
 # launch the jenkins instance using ami : you can change this ami id with your own ami. 
 resource "aws_instance" "jenkins_ec2_instance" {
-  ami                    = "ami-064bd5050ca35013f"
+  ami                    = "ami-02f9488dc7600d34e"
   instance_type          = "t2.medium"
   vpc_security_group_ids = [aws_security_group.jenkins_security_gp.id]
   key_name               = aws_key_pair.instance_key.key_name
